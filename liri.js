b15da7fe8f28989221ef4482a6c201e0ae20f7f9
rgtalbot <rgtalbot@users.noreply.github.com>
2016-09-01 16:16:43 -0400

    hw completed

---
+++ liri.js
@@ -0,0 +1,144 @@
+var inquirer = require('inquirer');
+var fs = require('fs');
+var Twitter = require('twitter');
+var Spotify = require('spotify-web-api-node');
+var request = require('request');
+var keys = require('./keys.js');
+
+var command = process.argv[2];
+
+inquirer.prompt([
+    {
+        type: "list",
+        message: "Choose Command",
+        choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+        name: "choice"
+    },
+]).then(function (prompt) {
+    switchFunction(prompt.choice);
+});
+
+function switchFunction(expression) {
+    switch (expression) {
+        case "my-tweets":
+            twitter();
+            break;
+        case "spotify-this-song":
+            spotify();
+            break;
+        case "movie-this":
+            movie();
+            break;
+        case "do-what-it-says":
+            itSays();
+            break;
+        default:
+            console.log('invalid entry');
+    }
+}
+
+
+function twitter() {
+    var client = new Twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
+    });
+    client.get('statuses/user_timeline', function (error, tweets) {
+        if (error) {
+            console.log(error);
+        }
+
+        console.log("-------START-------");
+        for (var i in tweets) {
+            console.log("-------------");
+            console.log("Date: " + tweets[i].created_at);
+            console.log("Tweet: " + tweets[i].text);
+            console.log("Retweeted: " + tweets[i].retweet_count);
+            console.log("Favorited: " + tweets[i].favorite_count);
+        }
+
+        console.log("--------END-------")
+
+    })
+}
+
+function spotify() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter song:',
+            name: "song",
+            default: parameter
+        }
+    ]).then(function (song) {
+        var spotifyApi = new Spotify({
+            clientID: keys.spotifyKeys.client_id,
+            clientSecret: keys.spotifyKeys.client_secret
+        });
+
+        spotifyApi.searchTracks(song.song, {limit: 5}).then(function (data) {
+            var tracks = data.body.tracks.items;
+            console.log("-------START-------");
+            for (var i in tracks) {
+                console.log("-------------");
+                console.log("Artist: " + tracks[i].artists[0].name);
+                console.log("Song: " + tracks[i].name);
+                console.log("Preview: " + tracks[i].preview_url);
+                console.log("Album: " + tracks[i].album.name);
+            }
+            console.log("--------END-------");
+        });
+    });
+}
+
+function movie() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter movie title: ',
+            name: 'title',
+            default: movieT
+        }
+    ]).then(function (movie) {
+        console.log(movie.title);
+
+        var query_url = "http://www.omdbapi.com/?t=" + movie.title + "&y=&plot=long&tomatoes=true&r=json";
+
+        request(query_url, function (error, data, body) {
+            if (error) {
+                console.log(error)
+            }
+            console.log("-------START-------");
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Date: " + JSON.parse(body).Released);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language(s): " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+            console.log("--------END-------");
+
+        });
+    });
+}
+
+
+var prompt,
+    parameter = "The Sign",
+    movieT = "Mr. Nobody";
+
+function itSays() {
+    fs.readFile("random.txt", "utf8", function(error, random) {
+       var randomTxt = random.split(',');
+        prompt = randomTxt[0];
+        parameter = randomTxt[1];
+        movie = randomTxt[1];
+        console.log('prompt', prompt);
+        console.log('parameter', parameter);
+        switchFunction(prompt);
+    });
+}
